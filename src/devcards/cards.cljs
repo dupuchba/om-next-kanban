14a373b449314e8810e60b9b7752f18f4b658834
baptistedupuch <baptiste.dupuch@gmail.com>
2017-03-13 13:12:36 +0100

    When applied, this commit will add :
    - devcards cards
    - React cards component

--- src/devcards/cards.cljs
+++ src/devcards/cards.cljs
@@ -1,9 +1,28 @@
 (ns devcards.cards
-  (:require-macros
-    [devcards.core :refer [defcard]])
-  (:require [om.next :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+  (:require [devcards.core :as dc :refer-macros [defcard]]
+            [om.next :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [om-next-kanban.components.card :as kanban-card]))
+
+(defcard
+  "# Kanban cards
+
+   Examples demonstrating how Kanban cards look given different properties
+   or layout constraints.")
+
+(defcard
+  "## Properties
+
+  Kanban cards are identified by IDs, have a text and assignees. But what
+  happens if some of these properties are missing? And what happens if there
+  are many assignees?")
+
+(defcard
+  "### Card with no id, text and assignees"
+  (fn [props _] (kanban-card/card @props))
+  {}
+  {:inspect-data true})
 
 (defcard my-first-card
          (dom/div nil
-                  (dom/h1 nil "Devcards")))
+                  (dom/h1 nil "Devcarssds")))
