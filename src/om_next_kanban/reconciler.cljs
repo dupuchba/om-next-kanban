8a0de9966d23d9faa8c47970580d58554da40d55
baptistedupuch <baptiste.dupuch@gmail.com>
2017-03-13 17:09:00 +0100

    When applied, this commit will add :
    - Addition reconciler

---
+++ src/om_next_kanban/reconciler.cljs
@@ -0,0 +1,16 @@
+(ns om-next-kanban.reconciler
+  (:require [goog.object :as gobj]
+            [om.next :as om]
+            [om-next-kanban.state :refer [initial-state]]))
+
+(defmulti read om/dispatch)
+
+(defmulti mutate om/dispatch)
+
+(def parser
+  (om/parser {:read read :mutate mutate}))
+
+(def reconciler
+  (om/reconciler {:state initial-state
+                  :parser parser}))
+
